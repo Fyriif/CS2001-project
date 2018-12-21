7c8b534ef14d4659d84f75db7e4cbd9804aec79d
Diogo Costa <hello@diogothecoder.com>
2018-12-21 20:19:55 +0000

    Connect setup screens (#5)
    * Connected welcome and login screen together
    
    * Implemented splash screen

---
+++ app/src/main/java/com/brunel/group30/fitnessapp/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.brunel.group30.fitnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        this.startBtn = (Button) findViewById(R.id.button_start);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
