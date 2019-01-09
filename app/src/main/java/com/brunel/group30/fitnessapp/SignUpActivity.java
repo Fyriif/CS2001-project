9cd1f4729164ef9211101abdb030b2febba14ffe
Diogo Costa <hello@diogothecoder.com>
2019-01-09 18:54:55 +0000

    Setting up screens validation (#13)
    * Fixed bug, dob selection now appearing in EditText
    
    * Added validation for first fragment
    
    * Added validation for sex fragment
    
    * Added validation for disabilities fragment
    
    * Added validation for location and times fragment

--- app/src/main/java/com/brunel/group30/fitnessapp/SignUpActivity.java
+++ app/src/main/java/com/brunel/group30/fitnessapp/SignUpActivity.java
@@ -1,7 +1,7 @@
 package com.brunel.group30.fitnessapp;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,19 +13,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mcsoft.timerangepickerdialog.RangeTimePickerDialog;
 
-import org.w3c.dom.Text;
-
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class SignUpActivity extends AppCompatActivity implements RangeTimePickerDialog.ISelectedTime {
 
-    public static final String TIMERANGEPICKER_TAG = "timerangepicker";
-    final Calendar calendar = Calendar.getInstance();
+    static final String TIMERANGEPICKER_TAG = "timerangepicker";
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -42,6 +44,11 @@ public class SignUpActivity extends AppCompatActivity implements RangeTimePicker
      */
     private ViewPager mViewPager;
 
+    private Calendar calendar;
+    private Format dateFormat;
+    private Locale defaultLocale;
+    private EditText dobEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,11 +62,106 @@ public class SignUpActivity extends AppCompatActivity implements RangeTimePicker
         mViewPager = (ViewPager) findViewById(R.id.view_pager_sign_up);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        //TypedArray ta = obtainStyledAttributes(R.style.TimePickerWidgetStyle);
+        calendar = Calendar.getInstance();
+        defaultLocale = Locale.getDefault();
+        dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
     }
 
     public void nextFragment(View v) {
-        this.mViewPager.setCurrentItem(this.mViewPager.getCurrentItem() + 1);
+        switch (this.mViewPager.getCurrentItem() + 1) {
+            case 1:
+                EditText forenameEditText = (EditText) findViewById(R.id.edit_text_forename);
+                EditText surnameEditText = (EditText) findViewById(R.id.edit_text_surname);
+                EditText dobEditText = (EditText) findViewById(R.id.edit_text_dob);
+
+                boolean forenameValid = false, surnameValid = false, dobValid = false;
+
+                if (forenameEditText.getText().toString().isEmpty()) {
+                    forenameEditText.setError(getString(R.string.error_field_empty));
+                } else {
+                    forenameEditText.setError(null);
+                    forenameValid = true;
+                }
+
+                if (surnameEditText.getText().toString().isEmpty()) {
+                    surnameEditText.setError(getString(R.string.error_field_empty));
+                } else {
+                    surnameEditText.setError(null);
+                    surnameValid = true;
+                }
+
+                if (dobEditText.getText().toString().isEmpty()) {
+                    dobEditText.setError(getString(R.string.error_field_empty));
+                } else {
+                    dobEditText.setError(null);
+                    dobValid = true;
+                }
+
+                if (forenameValid && surnameValid && dobValid) {
+                    this.mViewPager.setCurrentItem(this.mViewPager.getCurrentItem() + 1);
+                }
+
+                break;
+
+            case 2:
+                RadioButton maleRadioButton = (RadioButton) findViewById(R.id.button_sex_male);
+                RadioButton femaleRadioButton = (RadioButton) findViewById(R.id.button_sex_female);
+
+                if (!maleRadioButton.isChecked() && !femaleRadioButton.isChecked()) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_option_is_required), Toast.LENGTH_SHORT).show();
+                } else {
+                    this.mViewPager.setCurrentItem(this.mViewPager.getCurrentItem() + 1);
+                }
+
+                break;
+
+            case 5:
+                RadioButton yesDisabilityRadioButton = (RadioButton) findViewById(R.id.button_disability_yes);
+                RadioButton noDisabilityRadioButton = (RadioButton) findViewById(R.id.button_disability_no);
+
+                if (!yesDisabilityRadioButton.isChecked() && !noDisabilityRadioButton.isChecked()) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_option_is_required), Toast.LENGTH_SHORT).show();
+                } else {
+                    this.mViewPager.setCurrentItem(this.mViewPager.getCurrentItem() + 1);
+                }
+
+                break;
+
+            case 6:
+                CheckBox gymLocationCheckBox = (CheckBox) findViewById(R.id.button_gym);
+                CheckBox homeLocationCheckBox = (CheckBox) findViewById(R.id.button_home);
+                CheckBox parkLocationCheckBox = (CheckBox) findViewById(R.id.button_park);
+
+
+                if (!gymLocationCheckBox.isChecked() && !homeLocationCheckBox.isChecked() &&
+                        !parkLocationCheckBox.isChecked()) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_option_is_required), Toast.LENGTH_SHORT).show();
+                } else {
+                    this.mViewPager.setCurrentItem(this.mViewPager.getCurrentItem() + 1);
+                }
+
+                break;
+
+            case 7:
+                CheckBox mondayCheckBox = (CheckBox) findViewById(R.id.button_day_monday);
+                CheckBox tuesdayCheckBox = (CheckBox) findViewById(R.id.button_day_tuesday);
+                CheckBox wednesdayCheckBox = (CheckBox) findViewById(R.id.button_day_wednesday);
+                CheckBox thursdayCheckBox = (CheckBox) findViewById(R.id.button_day_thursday);
+                CheckBox fridayCheckBox = (CheckBox) findViewById(R.id.button_day_friday);
+
+                if (!mondayCheckBox.isChecked() && !tuesdayCheckBox.isChecked() && !wednesdayCheckBox.isChecked()
+                        && !thursdayCheckBox.isChecked() && !fridayCheckBox.isChecked()) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.error_option_is_required), Toast.LENGTH_SHORT).show();
+                } else {
+                    // TODO: go to dashboard
+                }
+
+                break;
+
+            default:
+                this.mViewPager.setCurrentItem(this.mViewPager.getCurrentItem() + 1);
+                break;
+        }
     }
 
     public void previousFragment(View v) {
@@ -67,13 +169,17 @@ public class SignUpActivity extends AppCompatActivity implements RangeTimePicker
     }
 
     public void enterDateOfBirth(View v) {
+        this.dobEditText = v.findViewById(R.id.edit_text_dob);
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @SuppressLint("SimpleDateFormat")
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
                 calendar.set(Calendar.YEAR, year);
                 calendar.set(Calendar.MONTH, monthOfYear);
                 calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                dobEditText.setText(new SimpleDateFormat(((SimpleDateFormat) dateFormat).toLocalizedPattern()).format(calendar.getTime()));
             }
 
         };
