d92b5d5bc5ad283beaeccd49d555418edba2fac1
Diogo Costa <hello@diogothecoder.com>
2019-01-29 17:04:35 +0000

    Tab dashboards (#31)
    * Create dashboard template
    
    * Added more options in bottom dashboard navigation and created a rough calendar view.
    
    * Refactored

--- app/src/main/java/com/brunel/group30/fitnessapp/DashboardActivity.java
+++ app/src/main/java/com/brunel/group30/fitnessapp/DashboardActivity.java
@@ -1,27 +1,41 @@
 package com.brunel.group30.fitnessapp;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.ViewFlipper;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    private ViewFlipper dashboardViewFlipper;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+                switch (item.getItemId()) {
+                    case R.id.navigation_dashboard_home:
+                        dashboardViewFlipper.setDisplayedChild(0);
+                        return true;
+                    case R.id.navigation_dashboard_calendar:
+                        dashboardViewFlipper.setDisplayedChild(1);
+                        return true;
+                    case R.id.navigation_dashboard_notifications:
+                        dashboardViewFlipper.setDisplayedChild(2);
+                        return true;
+                    case R.id.navigation_dashboard_account:
+                        dashboardViewFlipper.setDisplayedChild(3);
+                        return true;
+                }
+                return false;
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        this.mAuth = FirebaseAuth.getInstance();
-        if (this.mAuth.getCurrentUser() == null) {
-            nextActivity(LoginActivity.class);
-        }
+        dashboardViewFlipper = findViewById(R.id.view_dashboard);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    void nextActivity(Class nextActivity) {
-        startActivity(new Intent(getApplicationContext(), nextActivity));
-    }
 }
