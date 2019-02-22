5b077792f7903379106daa8d067b14a4bc17c473
Ahmad Aasim ( AJ ) <ahmadaasim@hotmail.com>
2019-02-22 11:55:32 +0000

    History Api (#51)
    * Added recording api
    
    * added history api
    
    * removed unecessary code
    
    * removed strings.xml

--- app/src/main/java/com/brunel/group30/fitnessapp/DashboardActivity.java
+++ app/src/main/java/com/brunel/group30/fitnessapp/DashboardActivity.java
@@ -9,38 +9,32 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import com.brunel.group30.fitnessapp.Models.UserInfo;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.data.DataPoint;
-import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
-import com.google.android.gms.fitness.data.Value;
-import com.google.android.gms.fitness.request.DataSourcesRequest;
-import com.google.android.gms.fitness.request.OnDataPointListener;
-import com.google.android.gms.fitness.request.SensorRequest;
-import com.google.android.gms.fitness.result.DataSourcesResult;
+import com.google.android.gms.fitness.result.DailyTotalResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.auth.User;
 import com.google.gson.Gson;
 
-import java.util.concurrent.TimeUnit;
-
 public class DashboardActivity extends AppCompatActivity
-        implements OnDataPointListener, GoogleApiClient.ConnectionCallbacks,
+        implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
     private static final int REQUEST_OAUTH = 1;
     private static final String OAUTH_PENDING = "auth_pending";
     private boolean authInProgress = false;
+
+    private TextView stepCountTextView;
 
     private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth mAuth;
@@ -73,6 +67,8 @@ public class DashboardActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        this.stepCountTextView = (TextView) findViewById(R.id.text_view_step_count);
+
         this.mAuth = FirebaseAuth.getInstance();
 
         dashboardViewFlipper = findViewById(R.id.view_dashboard);
@@ -84,10 +80,12 @@ public class DashboardActivity extends AppCompatActivity
         }
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Fitness.SENSORS_API)
+                .addApi(Fitness.HISTORY_API)
                 // Optional: specify more APIs used with additional calls to addApi
                 .useDefaultAccount()
                 .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addScope(Fitness.SCOPE_LOCATION_READ_WRITE)
+                .addScope(Fitness.SCOPE_BODY_READ_WRITE)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
@@ -105,42 +103,23 @@ public class DashboardActivity extends AppCompatActivity
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
-                .setDataTypes(DataType.TYPE_STEP_COUNT_CUMULATIVE)
-                .setDataSourceTypes(DataSource.TYPE_DERIVED)
-                .build();
-
-        ResultCallback<DataSourcesResult> dataSourcesResultCallback = result -> {
-            for (DataSource data : result.getDataSources()) {
-                if (DataType.TYPE_STEP_COUNT_CUMULATIVE.equals(data.getDataType())) {
-                    onDataListener(data, DataType.TYPE_STEP_COUNT_CUMULATIVE);
+        PendingResult<DailyTotalResult> result = Fitness.HistoryApi.readDailyTotal(mGoogleApiClient,
+                DataType.AGGREGATE_STEP_COUNT_DELTA);
+        result.setResultCallback(dailyTotalResult -> {
+            if (dailyTotalResult.getStatus().isSuccess()) {
+                DataSet totalSet = dailyTotalResult.getTotal();
+                if (totalSet != null && !totalSet.isEmpty()) {
+                    this.stepCountTextView.setText(totalSet.getDataPoints().get(0)
+                            .getValue(Field.FIELD_STEPS).toString());
                 }
             }
-        };
-
-        Fitness.SensorsApi.findDataSources(mGoogleApiClient, dataSourceRequest)
-                .setResultCallback(dataSourcesResultCallback);
-    }
-
-    private void onDataListener(DataSource dataSource, DataType dataType) {
-
-        SensorRequest fitness = new SensorRequest.Builder()
-                .setDataSource(dataSource)
-                .setDataType(dataType)
-                .setSamplingRate(1, TimeUnit.SECONDS)
-                .build();
-
-        Fitness.SensorsApi.add(mGoogleApiClient, fitness, this)
-                .setResultCallback(status -> {
-                    if (status.isSuccess()) {
-                        Log.e("GoogleFit", "SensorApi successfully added");
-                    }
-                });
+        });
     }
 
     @Override
     public void onConnectionSuspended(int i) {
         // The connection has been interrupted. Wait until onConnected() is called.
+        Log.e("HistoryAPI", "onConnectionSuspended");
     }
 
     @Override
@@ -192,29 +171,7 @@ public class DashboardActivity extends AppCompatActivity
             }
         } else {
             Log.e("FIT", "authInProgress");
+            Log.e("HistoryAPI", "onConnectionFailed");
         }
-    }
-
-    @Override
-    public void onDataPoint(DataPoint dataPoint) {
-        for (final Field field : dataPoint.getDataType().getFields()) {
-            final Value value = dataPoint.getValue(field);
-            runOnUiThread(() -> Toast.makeText(getApplicationContext(),
-                    "Field: " + field.getName() + " Value: " +
-                            value, Toast.LENGTH_SHORT).show());
-        }
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Fitness.SensorsApi.remove(mGoogleApiClient, this)
-                .setResultCallback(status -> {
-                    if (status.isSuccess()) {
-                        mGoogleApiClient.disconnect();
-                    }
-                });
     }
 }
