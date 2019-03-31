1bd0a071f67b29dbdca5d0c340402751aa4c82d4
Diogo Costa <hello@diogothecoder.com>
2019-03-31 16:34:25 +0100

    Weight field in Goals class now has default value

--- app/src/main/java/com/brunel/group30/fitnessapp/Models/Goals.kt
+++ app/src/main/java/com/brunel/group30/fitnessapp/Models/Goals.kt
@@ -4,10 +4,11 @@ import com.brunel.group30.fitnessapp.Services.CustomFirebaseFirestoreService
 import com.brunel.group30.fitnessapp.Utils.Exceptions
 import com.google.firebase.firestore.PropertyName
 
-data class Goals(@PropertyName("steps_target") var stepsTarget: Int = 10000,
-                 @PropertyName("weight_target") var weightTarget: Int,
-                 @PropertyName("hydration_target") var hydrationTarget: Int = 3*1000,
-                 @PropertyName("calorie_target") var calorieTarget: Int = 100) {
+data class Goals(@get:PropertyName("steps_target") @set:PropertyName("steps_target") var stepsTarget: Int = 10000,
+                 @get:PropertyName("weight_target") @set:PropertyName("weight_target") var weightTarget: Int = 60,
+                 @get:PropertyName("hydration_target") @set:PropertyName("hydration_target") var hydrationTarget: Int = 3000,
+                 @get:PropertyName("calorie_target") @set:PropertyName("calorie_target") var calorieTarget: Int = 100) {
+
     companion object {
         const val COLLECTION_NAME = "goals"
         const val PREF_KEY_NAME = "goals"
